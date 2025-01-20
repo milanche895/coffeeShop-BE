cf75da5a4e91be4514e47f57365d1b461f5850ed
milanche895 <milanstojanovic895@gmail.com>
2025-01-20 12:42:51 +0100

    change name to shop

--- src/main/java/com/project/brka_media/constants/WebConstants.java
+++ src/main/java/com/project/brka_media/constants/WebConstants.java
@@ -2,8 +2,8 @@ package com.project.brka_media.constants;
 
 public class WebConstants {
 
-    public static final String BASE_ADMIN_URL = "/auth/media/admin";
-    public static final String BASE_CLIENT_URL = "/auth/media/client";
-    public static final String PUBLIC_BASE_URL = "/public/media";
+    public static final String BASE_ADMIN_URL = "/auth/shop/admin";
+    public static final String BASE_CLIENT_URL = "/auth/shop/client";
+    public static final String PUBLIC_BASE_URL = "/public/shop";
 
 }
